1a81fbce25fe1739790ef3f3d18d102872deade3
Anthony Kholkin <kholkin.anthony@gmail.com>
2024-12-19 20:45:08 +0300

    + RETAIL_PRICE_TYPE to get_good_groups API

--- src/api/v1/good_group.py
+++ src/api/v1/good_group.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, status, Depends, Query
+
+from core.constants import RETAIL_PRICE_TYPE
 from schemas.good_group import GetTreeGoodGroupSchema
 from services.web.good_group import GoodGroupService
 
@@ -6,7 +8,7 @@ router = APIRouter(prefix="/good-groups", tags=["Группы товаров"])
 
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=list[GetTreeGoodGroupSchema])
-async def get_good_by_id(
-    good_service: GoodGroupService = Depends(), price_type_guid: str = Query(...)
+async def get_good_groups(
+    good_service: GoodGroupService = Depends(), price_type_guid: str = Query(default=RETAIL_PRICE_TYPE)
 ) -> list[GetTreeGoodGroupSchema]:
     return await good_service.get_available_good_groups(price_type_guid=price_type_guid)
